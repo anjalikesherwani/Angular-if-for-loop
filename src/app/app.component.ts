c8cb895a279b17fc8e1ae9d11652d91fbf04050b
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-02-14 16:25:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FooterComponent,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  // encapsulation:ViewEncapsulation.None
+  // encapsulation:ViewEncapsulation.ShadowDom,
+  encapsulation:ViewEncapsulation.Emulated,
+
+
 })
 export class AppComponent {
-  title = 'Directives';
+  title = 'Directives'
+
+  sdata:any = [{Roll_No:1,
+    name:'John Doe',
+    age:32,
+    gender:'Male',loc:'Ald'
+  },
+  {Roll_No:2,
+    name:'Anjali ',
+    age:24,
+    gender:'Female',loc:'Ald'
+  },
+  {Roll_No:3,
+    name:'jyoti',
+    age:25,
+    gender:'Female',loc:'Patan'
+  },
+  {Roll_No:4,
+    name:'vaishu',
+    age:22,
+    gender:'Female',loc:'Kanpur'
+  },
+  {Roll_No:5,
+    name:'Ashish',
+    age:25,
+    gender:'Male',loc:'Ald'
+  }
+];
+
+  // xname:string='Anjali'
+  // hello:boolean=false
+
+
+  xlogin:any;
+  hello(){
+    this.xlogin=true
+  }
+
 }
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+
+  inputs:['uname'],
+
+})
+export class FooterComponent {
+// age:any;
+uname:any;
+}
